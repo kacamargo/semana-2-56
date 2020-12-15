8d0bb886baa08699879fce04b4e0c381c6466d5f
Karen Camargo <kjcamargo19@gmail.com>
2020-12-15 16:45:59 -0500

    Personalización estilos Team

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div id="app">
 
 <div class="container-fluid">
-    <div class="row justify-content-center mb-5">
-      <div class="col mt-5" v-for="(item, index) of member" :key="index"> 
+  <h6 class="display-2 text-center" style="font-family: 'Roboto', sans-serif;">Conoce Nuestro Equipo</h6>
+  <p class="text-muted text-center lead mb-0">¡Creemos sinceramente que la tecnología tiene un potencial casi ilimitado para cambiar nuestro mundo!</p>
+    <div class="row justify-content-center mb-5 d-flex">
+      <div class="col-md mt-4 d-flex justify-content-around" v-for="(item, index) of member" :key="index"> 
         <team-card v-bind:member="item"></team-card>
 
       </div>
@@ -27,41 +29,40 @@ data(){
     member:[
       {
         codigo: 1,
-        nombre: "Karen Julieth Camargo Flórez",
-        descripcion: "Ingeniera electrónica y Tecnologa en electrónica Industrial de las Unidades Tecnológicas de Santander," 
-        + " entre sus pasatiempos favoritos se encuentra la fotografía y prácticar volleyball 🏐. Reside actualmente en la ciudad de Bucaramanga",
+        nombre: "Karen Julieth Camargo Florez",
+        descripcion: "Ingeniera Electrónica y Tecnóloga en Electrónica Industrial de las Unidades Tecnológicas de Santander," 
+        + " entre sus pasatiempos favoritos se encuentra la fotografía y prácticar voleibol.",
         rol: "Arquitecto de Software",
         imagen:"https://imgur.com/0r53PYm.jpg",
       },
       {
         codigo: 2,
         nombre: "Manuel José Hernández Rivas",
-        descripcion: "Estudiante de ingeniería en sistemas computacionales y telemática. Programador java y desarrollador de"
-        + " aplicaciones android nativas. Entré los pasatiempos están la lectura, la fotografía y el arte digital.",
+        descripcion: "Estudiante de Ingeniería en Sistemas Computacionales y Telemática. Programador Java y desarrollador de"
+        + " aplicaciones Android nativas. Entré los pasatiempos están la lectura, la fotografía y el arte digital.",
         rol:"Desarrollador Backend",
         imagen:"https://imgur.com/c4VZpFz.jpg",
       },
       {
         codigo: 3,
-        nombre: "Team J",
-        descripcion: "Ingeniero Mecatronico, Nacido en Barrancabermeja, Santander,"
-        + "Colombia curioso de nacimiento y apasionado por los videojuegos",
-        rol:"Tester",
+        nombre: "",
+        descripcion: "'Ninguno de nosotros es tan inteligente como todos nosotros' - Ken Blanchard",
+        rol:"",
         imagen:"https://imgur.com/6dUC1Ub.jpg",
       },
       {
         codigo: 4,
         nombre: "Alex David Ospina Gonzalez",
         descripcion: "Estudiante de Español y Filología Clásica de la Universidad Nacional de Colombia," 
-        + " tecnólogo en Gestión Bancaria y de Entidades Financieras del Servicio Nacional de Aprendizaje - SENA, gestor de prevención de fraudes del Banco Falabella Colombia.",
+        + " Tecnólogo en Gestión Bancaria y de Entidades Financieras del Servicio Nacional de Aprendizaje - SENA, Gestor de prevención de fraudes del Banco Falabella Colombia.",
         rol:"Desarrollador Backend",
         imagen:"https://imgur.com/xJZp92x.jpg",
       },
       {
         codigo: 5,
         nombre: "Julio Cesar Cuervo Diaz",
-        descripcion: "Ingeniero Mecatronico - Universidad Santo Tomas Bucaramanga, Nacido en Barrancabermeja, Santander,"
-        + "Colombia curioso de nacimiento y apasionado por los videojuegos",
+        descripcion: "Ingeniero Mecatrónico - Universidad Santo Tomás Bucaramanga, Nacido en Barrancabermeja, Santander,"
+        + "Colombia. Curioso de nacimiento y apasionado por los videojuegos.",
         rol:"Desarrollador Frontend",
         imagen:"https://imgur.com/QCkZqVk.jpg",
       },
@@ -71,4 +72,3 @@ data(){
 }
 
 </script>
-
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,18 +1,16 @@
 <template>
-
-    <div class="card border-dark bg-light" style="height: 38em">
+    <div class="card shadow">
         <div class="d-flex justify-content-center p-2" style=" margin-top:1.5em">
-            <img v-bind:src="member.imagen" alt="Team photo shoot" class="rounded">
+            <img v-bind:src="member.imagen" alt="Team photo shoot" class="rounded img-fluid">
         </div>
 
         <div class="card-body">
-            <p class="card-title text-center h6 text-uppercase">{{ member.nombre }}</p>
-            <p class="card-title text-center h6 text-uppercase"><small class="text-muted">{{ member.rol }}</small></p>
-            <br>
-            <p class="card-text text-center h6 "><small>{{ member.descripcion }}</small></p>
+            <p class="card-title text-center h4 text-uppercase mb-3" style="color: #78344c; font-family:'Bebas Neue', cursive;">{{ member.nombre }}</p>
+            <div class=" mx-auto" style="width: 30%; height: 2px; background: #78344c; "></div>
+            <p class="card-text text-center h6 py-2" style=""><small>{{ member.descripcion }}</small></p>
         </div>
-        <div class="card-footer text-center">
-            <small class="text-muted">12 - Dic - 2020</small>
+        <div class="card-footer text-uppercase my-0" style="background:#78344c; height:3em">
+            <p class= "text-center m-0 small" style="color: #ffffff;" >{{ member.rol}}</p>
         </div>
     </div>
 </template>
@@ -27,6 +25,4 @@
 </script>
 
 <style scoped>
-
-
 </style>
