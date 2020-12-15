81a16de4c26d4a6ef3b3622b82cc5c494da8a185
adospinag <adospinag@unal.edu.co>
2020-12-14 21:05:07 -0500

    con servicios

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <services-section> </services-section>
   </div>
+  
 </template>
 
 <script>
-
+import ServicesSection from './components/ServicesSection.vue'
+import ServicesSection from "./components/ServicesSection.vue"
 export default {
   name: 'App',
+  components: {
+  ServicesSection
+  }
 }
 </script>
 
