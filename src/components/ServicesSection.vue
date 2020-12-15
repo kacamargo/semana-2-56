e4d2a1a69a4fde8d130df54b7f9af9f182d7c45b
adospinag <adospinag@unal.edu.co>
2020-12-15 00:06:06 -0500

    primer final

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -1,55 +1,56 @@
-<template>
+<template> 
   <!-- services -->
   <div class="container-fluid">
-    <div class="row justify-content-center mb-5">
-      <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
-        <div class="card">
-          <div class="d-flex justify-content-center p-2">
-            <img src="https://www.noticiasensalud.com/wp-content/uploads/2020/04/Un-decalogo-para-cuidarse-en-familia-durante-la-pandemia-de-coronavirus-1.jpg" alt="Seguro de vida" height="200px" />
-          </div>
-          <div class="card-body">
-            <h4 class="card-title">Seguro de vida</h4>
-            <p class="card-text">
-              Porque sabemos lo importante que eres para todos los tuyos...
-            </p>
-            <a href="https://web.segurosfalabella.com/co/seguros-de-vida/con-devolucion/" class="btn btn-primary" target="_blank">Ver más:</a>
-          </div>
+        <h1 id="services"><center><strong> Nuestros servicios </strong></center></h1>
+        <div class="row justify-content-center mb-5">
+            <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
+                <div class="card bg-light border border-info">
+                    <div class="d-flex justify-content-center p-2">
+                        <img style="border: 1px solid; color: #17A2BB" src="https://www.noticiasensalud.com/wp-content/uploads/2020/04/Un-decalogo-para-cuidarse-en-familia-durante-la-pandemia-de-coronavirus-1.jpg" alt="Seguro de vida" height="200px" width="350px"/>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title text-info"><strong> Seguro de vida </strong></h4>
+                        <p class="card-text">
+                        Porque sabemos lo importante que eres para todos los tuyos...
+                        </p>
+                        <a href="https://web.segurosfalabella.com/co/seguros-de-vida/con-devolucion/" class="btn btn-success" target="_blank">Ver más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
+                <div class="card bg-light border border-info">
+                    <div class="d-flex justify-content-center p-2">
+                        <img style="border: 1px solid; color: #17A2BB" src="https://cdn.aarp.net/content/dam/aarp/auto/2017/07/1140-hyundai-ioniq-great-cars-road-trips-esp.imgcache.revfd63981c76a67e8a4ed28622bb76883e.web.700.403.jpg" alt="Seguro de vehículo" height="200px" width="350px"/>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title text-info"><strong> Seguro de vehículo </strong></h4>
+                        <p class="card-text">
+                        Porque sabemos que tu patrimonio y tus bienes son valiosos...
+                        </p>
+                        <a href="https://auto.segurosfalabella.com.co/todo-riesgo/chat-lia" class="btn btn-success" target="_blank">Ver más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
+                <div class="card bg-light border border-info">
+                    <div class="d-flex justify-content-center p-2">
+                        <img style="border: 1px solid; color: #17A2BB" src="https://inspecciontecnicadeestanterias.com/wp-content/uploads/2019/05/Obligaciones-del-empresario-y-del-trabajador-en-materia-de-seguridad-en-el-almacen.jpg" alt="Seguro de desempleo" height="200px" width="350px"/>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title text-info"><strong> Seguro de desempleo </strong></h4>
+                        <p class="card-text">
+                        Porque sabemos que no estás exento(a) de algún imprevisto laboral...
+                        </p>
+                        <a href="https://web.segurosfalabella.com/co/otros-seguros/seguro-de-desempleo/" class="btn btn-success" target="_blank">Ver más</a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
-        <div class="card">
-          <div class="d-flex justify-content-center p-2">
-            <img src="https://cdn.aarp.net/content/dam/aarp/auto/2017/07/1140-hyundai-ioniq-great-cars-road-trips-esp.imgcache.revfd63981c76a67e8a4ed28622bb76883e.web.700.403.jpg" alt="Seguro de vehículo" height="200px" />
-          </div>
-          <div class="card-body">
-            <h4 class="card-title">Seguro de vehículo</h4>
-            <p class="card-text">
-              Porque sabemos que tu patrimonio y tus bienes son valiosos...
-            </p>
-            <a href="https://auto.segurosfalabella.com.co/todo-riesgo/chat-lia" class="btn btn-primary" target="_blank">Ver más:</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
-        <div class="card">
-          <div class="d-flex justify-content-center p-2">
-            <img src="https://inspecciontecnicadeestanterias.com/wp-content/uploads/2019/05/Obligaciones-del-empresario-y-del-trabajador-en-materia-de-seguridad-en-el-almacen.jpg" alt="Seguro de desempleo" height="200px" />
-          </div>
-          <div class="card-body">
-            <h4 class="card-title">Seguro de desempleo</h4>
-            <p class="card-text">
-              Porque sabemos que no estás exento(a) de algún imprevisto laboral...
-            </p>
-            <a href="https://web.segurosfalabella.com/co/otros-seguros/seguro-de-desempleo/" class="btn btn-primary" target="_blank">Ver más:</a>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ServicesSection",
+  name: "ServicesSection"
 };
 </script>
