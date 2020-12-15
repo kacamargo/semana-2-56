30945d0d2325f01a93a4d8f4666a9cbb3a5b3014
adospinag <adospinag@unal.edu.co>
2020-12-14 22:07:01 -0500

    ahí vamos

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -1,69 +1,55 @@
 <template>
-    <!-- services -->
-    <div class="container-fluid">
-        <div class="row justify-content-center mb-5">
-            <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
-
-                <div class="card">
-                    <div class="d-flex justify-content-center p-2">
-
-                        <svg width="12em" height="12em" viewBox="0 0 16 16" class="bi bi-alarm-fill" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z" />
-                        </svg>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
-                <div class="card">
-                    <div class="d-flex justify-content-center p-2">
-
-                        <svg width="12em" height="12em" viewBox="0 0 16 16" class="bi bi-alarm-fill" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z" />
-                        </svg>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
-                <div class="card">
-                    <div class="d-flex justify-content-center p-2">
-
-                        <svg width="12em" height="12em" viewBox="0 0 16 16" class="bi bi-alarm-fill" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z" />
-                        </svg>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
+  <!-- services -->
+  <div class="container-fluid">
+    <div class="row justify-content-center mb-5">
+      <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
+        <div class="card">
+          <div class="d-flex justify-content-center p-2">
+            <img src="https://www.noticiasensalud.com/wp-content/uploads/2020/04/Un-decalogo-para-cuidarse-en-familia-durante-la-pandemia-de-coronavirus-1.jpg" alt="Seguro de vida" height="150px" />
+          </div>
+          <div class="card-body">
+            <h4 class="card-title">Seguro de vida</h4>
+            <p class="card-text">
+              Porque sabemos lo importante que eres para todos los tuyos...
+            </p>
+            <a href="https://web.segurosfalabella.com/co/seguros-de-vida/con-devolucion/" class="btn btn-primary" target="_blank">Ver más:</a>
+          </div>
         </div>
+      </div>
+      <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
+        <div class="card">
+          <div class="d-flex justify-content-center p-2">
+            <img src="https://cdn.aarp.net/content/dam/aarp/auto/2017/07/1140-hyundai-ioniq-great-cars-road-trips-esp.imgcache.revfd63981c76a67e8a4ed28622bb76883e.web.700.403.jpg" alt="Seguro de vehículo" height="150px" />
+          </div>
+          <div class="card-body">
+            <h4 class="card-title">Seguro de vehículo</h4>
+            <p class="card-text">
+              Porque sabemos que tu patrimonio y tus bienes son valiosos...
+            </p>
+            <a href="https://auto.segurosfalabella.com.co/todo-riesgo/chat-lia" class="btn btn-primary" target="_blank">Ver más:</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
+        <div class="card">
+          <div class="d-flex justify-content-center p-2">
+            <img src="https://inspecciontecnicadeestanterias.com/wp-content/uploads/2019/05/Obligaciones-del-empresario-y-del-trabajador-en-materia-de-seguridad-en-el-almacen.jpg" alt="Seguro de desempleo" height="150px" />
+          </div>
+          <div class="card-body">
+            <h4 class="card-title">Seguro de desempleo</h4>
+            <p class="card-text">
+              Porque sabemos que no estás exento(a) de algún imprevisto laboral...
+            </p>
+            <a href="https://web.segurosfalabella.com/co/otros-seguros/seguro-de-desempleo/" class="btn btn-primary" target="_blank">Ver más:</a>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "ServicesSection"
-}
+  name: "ServicesSection",
+};
 </script>
