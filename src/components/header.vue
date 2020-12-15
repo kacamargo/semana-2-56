fd841b3adca07ab3af83285e82743c5f23403c21
manuelhzr <manuel_hzr@outlook.com>
2020-12-15 14:19:51 -0500

    crear elementos

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,11 +1,27 @@
 <template>
-  
+  <header class="site-header inicio">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="#">
+                    <img src="../assets/logoHeader.svg" alt="Logotipo alpha">
+                </a>
+                <nav class="navegacion">
+                    <a href="#">Inicio</a>
+                    <a href="#">Servicios</a>
+                    <a href="#">Noticias</a>
+                    <a href="#">Equipo</a>
+                </nav>
+            </div>
+
+            <h1>Lorem ipsum dolor sit, consectetur adipisicing elit. </h1>
+        </div> <!-- contenedor-->
+    </header>
 </template>
 
 <script>
 export default {
-
-}
+  name: "cabecera"
+};
 </script>
 
 <style>
