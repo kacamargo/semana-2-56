36832a5864fba9c4ab0d480d3c5d1dd6083e9fa8
manuelhzr <manuel_hzr@outlook.com>
2020-12-16 15:53:44 -0500

    creación slider

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,26 +1,76 @@
 <template>
   <header class="site-header inicio">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="#">
-                    <img src="../assets/logoHeader.svg" alt="Logotipo alpha">
-                </a>
-                <nav class="navegacion">
-                    <a href="#">Inicio</a>
-                    <a href="#">Servicios</a>
-                    <a href="#">Noticias</a>
-                    <a href="#">Equipo</a>
-                </nav>
-            </div>
+    <!-- contenedor-->
+    <div>
+      <transition-group  name="fade" tag="div">
+        <div class="caja" v-for="i in [currentIndex]" :key="i">
+          <img class="sl" :src="currentImg" />
+          <div class="texto" v-text="currentText"></div>
+        </div>
+      </transition-group>
 
-            <h1>Lorem ipsum dolor sit, consectetur adipisicing elit. </h1>
-        </div> <!-- contenedor-->
-    </header>
+      <div class="contenedor contenido-header">
+          <div class="barra">
+            <a href="#"> <img src="../assets/logoHeader.svg" alt="Logotipo alpha"> </a>
+            <nav class="navegacion">
+              <a href="#">Inicio</a>
+              <a href="#">Servicios</a>
+              <a href="#">Noticias</a>
+              <a href="#">Equipo</a>
+            </nav>
+          </div>
+        </div>
+
+      <a class="prev" @click="prev" href="#">&#10094;</a>
+      <a class="next" @click="next" href="#">&#10095;</a>
+    </div>
+  </header>
+
 </template>
 
 <script>
 export default {
-  name: "cabecera"
+  name: "cabecera",
+
+data() {
+    return {
+      images: [
+        "https://i.imgur.com/27nlDg0.jpg",
+        "https://i.imgur.com/vtHPO89.jpg",
+        "https://i.imgur.com/lJBbiBI.jpg",
+      ],
+      titles: ["Image 1", "Image 2", "Image 3",],
+      timer: null,
+      currentIndex: 0,
+    };
+  },
+
+  mounted: function () {
+    this.startSlide();
+  },
+
+  methods: {
+    startSlide: function () {
+      this.timer = setInterval(this.next, 6000);
+    },
+
+    next: function () {
+      this.currentIndex += 1;
+    },
+    prev: function () {
+      this.currentIndex -= 1;
+    },
+  },
+
+  computed: {
+    currentImg: function () {
+      return this.images[Math.abs(this.currentIndex) % this.images.length];
+    },
+    currentText: function () {
+      return this.titles[Math.abs(this.currentIndex) % this.images.length];
+    },
+  },
+
 };
 </script>
 
@@ -31,58 +81,139 @@ html{
     font-size: 62.5%; /** Reset para REMS -62.5% -10px de 16px **/
 }
 
-.contenedor{
-    max-width: 120rem;
-    margin: 0 auto;
+.caja {
+  position: relative;
+  height: max-content;
 }
 
-.site-header{
-    background-color: #333333;
-    padding: 1rem 0 3rem 0;
+.texto {
+  position: absolute;
+  top: 50%;
+  left: 10%;
+  font-size: 30px;
+  color: white;
+  text-shadow: 2px 2px 2px #333333;
 }
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.9s ease;
+  overflow: hidden;
+  visibility: visible;
+  position: absolute;
+  width: 100%;
+  opacity: 1;
+}
+
+.fade-enter,
+.fade-leave-to {
+  visibility: hidden;
+  width: 100%;
+  opacity: 0;
+}
+
+img {
+  height: 100%;
+  width: 100%;
+  position: center center;
+}
+
+.prev,
+.next {
+  cursor: pointer;
+  position: absolute;
+  top: 40%;
+  width: auto;
+  padding: 16px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.7s ease;
+  border-radius: 0 4px 4px 0;
+  text-decoration: none;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+}
+
+.prev {
+  left: 0;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover,
+.next:hover {
+  background-color: rgba(0, 0, 0, 0.9);
+}
+
+
+
+.contenedor {
+  min-width: 80%;
+  margin: 0 auto;
+  position: absolute;
+  top: 10px;
+  left: 10%;
+}
+
+.site-header {
+  background-color: #333333;
+  padding: 0 0 3rem 0;
+  position: relative;
+}
+
 
 .site-header.inicio{
-    background-image: url(../assets/header1.jpg);
+    /*background-image: url(../assets/header1.jpg);*/
     background-position: center center;
     background-size: cover;
     height: 100vh;
     min-height: 60rem;
+    max-height: 100vh;
 }
 
-.contenido-header{
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+.contenido-header {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
-.contenido-header h1{
-    color: white;
-    padding-bottom: 2rem;
-    max-width: 60rem;
-    line-height: 2;
+.contenido-header h1 {
+  color: white;
+  padding-bottom: 2rem;
+  max-width: 60rem;
+  line-height: 2;
 }
 
-.barra{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-top: 3rem;
+
+.barra {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 3rem;
+  align-items: flex-end;
+  text-shadow: 3px 3px 3px #333333;
 }
+
+
 
 /* Navegacion */
 .navegacion a {
-    color: white;
-    text-decoration: none;
-    font-size: 1.8rem;
-    margin-right: 2rem;
+  color: white;
+  text-decoration: none;
+  font-size: 1.8rem;
+  margin-right: 2rem;
 }
 
-.navegacion a:hover{
-    color: #71B100;
+.navegacion a:hover {
+  color: #333333;
 }
 
-.navegacion a:last-of-type{
-    margin: 0;
+.navegacion a:last-of-type {
+  margin: 0;
 }
 </style>
