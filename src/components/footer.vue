e75ecbbafb0ae849fb9dabf5adca414377c69560
manuelhzr <manuel_hzr@outlook.com>
2020-12-16 17:32:47 -0500

    Comment first footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,17 @@
 <template>
-  
+  <!-- <footer class="site-footer seccion">
+        <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.html">Nosotros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">Contacto</a>
+            </nav>
+
+            <p class="copyright">Todos los derechos reservados 2020 &copy;</p>
+        </div>
+  </footer> -->
+
 </template>
 
 <script>
