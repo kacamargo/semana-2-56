7b94fdff0571a3177977747015f6e9fe716b4859
manuelhzr <manuel_hzr@outlook.com>
2020-12-16 17:16:02 -0500

    Update App.vue
    Agregando presentación de footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <cabecera> </cabecera>
+    <pie> </pie>
   </div>
 </template>
 
 <script>
 
 import cabecera from "./components/header.vue";
+import pie from "./components/footer.vue";
 
 export default {
   name: "App",
   components: {
     cabecera,
+    pie,
   },
 };
 </script>
